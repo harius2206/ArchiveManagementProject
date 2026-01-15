d160d38fd1e127ae4949f3399d7bc8920fc83cfd
Hariuss <jenuaviotseh2202007@gmail.com>
2026-01-15 16:34:36 +0200

    2

--- ArchiveManagementProject/urls.py
+++ ArchiveManagementProject/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from archive_core import views
 
 urlpatterns = [
-    # За замовчуванням відкриваємо список документів 
-    path('', views.documents_info, name='index'), 
+    # Тепер корінь сайту показує головну сторінку index.html
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('documents/', views.documents_info, name='documents_info'),
 ]
--- archive_core/views.py
+++ archive_core/views.py
@@ -3,14 +3,14 @@ from .uow import DjangoUnitOfWork
 
 # Аналог HomeController.Index [cite: 3, 6]
 def index(request):
-    return render(request, 'archive_core/index.html')
+    return render(request, 'archivel_core/index.html')
 
 # Аналог HomeController.About
 def about(request):
-    return render(request, 'archive_core/about.html')
+    return render(request, 'archivel_core/about.html')
 
 # Аналог DocumentsController.ObjectsInfo
 def documents_info(request):
     uow = DjangoUnitOfWork()
     objects = uow.documents.get_all() # Отримуємо дані через репозиторій
-    return render(request, 'archive_core/objects_info.html', {'model': objects})
+    return render(request, 'archivel_core/_objects_info.html', {'model': objects})
